25491089ff417f4201e28a9404d97e8c46558f2c
Shobayo Samuel <shobayosamuel62@gmail.com>
2023-04-04 00:39:03 +0000

    Add: shellcheck

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,3 +1,8 @@
+#!/bin/bash
+
+# Use Puppet to automate the task of creating a custom HTTP header response
+
+cat << EOF > /etc/puppetlabs/code/environments/production/manifests/init.pp
 exec { 'update':
   command => '/usr/bin/apt-get update',
 }
@@ -15,13 +20,17 @@ file { '/etc/nginx/nginx.conf':
 file_line { 'http_header':
   path  => '/etc/nginx/nginx.conf',
   match => 'http {',
-  line  => "http {\n\tadd_header X-Served-By \"${hostname}\";",
+  line  => "http {\n\tadd_header X-Served-By \"\${hostname}\";",
   require => File['/etc/nginx/nginx.conf'],
   notify => Exec['restart_nginx'],
 }
 
 exec { 'restart_nginx':
-  command => '/usr/sbin/service nginx restart',
+  command     => '/usr/sbin/service nginx restart',
   refreshonly => true,
-  subscribe => File_line['http_header'],
+  subscribe   => File_line['http_header'],
 }
+EOF
+
+# Run puppet apply
+sudo /opt/puppetlabs/bin/puppet apply /etc/puppetlabs/code/environments/production/manifests/init.pp
