6f93351af18fa5f4dfa8ed33b919497b3e7c492b
Shobayo Samuel <shobayosamuel62@gmail.com>
2023-05-09 18:12:39 +0100

    fix: requirements

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,21 +1,17 @@
 #!/usr/bin/python3
+"""
+Contains the number_of_subscribers function
+"""
+
 import requests
-'''Get the number of subscribers from a given subreddit'''
 
 
 def number_of_subscribers(subreddit):
-    '''
-        return the number of subscribers from reddit api
-    '''
-    if subreddit is None or not isinstance(subreddit, str):
+    """returns the number of subscribers for a given subreddit"""
+    if subreddit is None or type(subreddit) is not str:
         return 0
-
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {'User-Agent': "Mozilla/5.0"}
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 200:
-        body = response.json()
-        subscribers = body['data']['subscribers']
-        return subscribers
-    else:
-        return 0
+    r = requests.get('http://www.reddit.com/r/{}/about.json'.format(subreddit),
+                     headers={'User-Agent': '0x16-api_advanced:project:\
+v1.0.0 (by /u/firdaus_cartoon_jr)'}).json()
+    subs = r.get("data", {}).get("subscribers", 0)
+    return subs
