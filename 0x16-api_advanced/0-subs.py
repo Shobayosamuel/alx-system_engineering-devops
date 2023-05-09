5273e505f623dbafb333034857aecaa73fd954ae
Shobayo Samuel <shobayosamuel62@gmail.com>
2023-05-09 18:08:31 +0100

    fix: requirement failures

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,6 +7,9 @@ def number_of_subscribers(subreddit):
     '''
         return the number of subscribers from reddit api
     '''
+    if subreddit is None or not isinstance(subreddit, str):
+        return 0
+
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {'User-Agent': "Mozilla/5.0"}
     response = requests.get(url, headers=headers, allow_redirects=False)
