b499071ddaff55c933d11bb9bfc7dfe2e1d37ad9
Shobayo Samuel <shobayosamuel62@gmail.com>
2023-05-09 18:01:51 +0100

    feat: get number of subscribers from reddit api

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+import requests
+'''Get the number of subscribers from a given subreddit'''
+
+
+def number_of_subscribers(subreddit):
+    '''
+        return the number of subscribers from reddit api
+    '''
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {'User-Agent': "Mozilla/5.0"}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 200:
+        body = response.json()
+        subscribers = body['data']['subscribers']
+        return subscribers
+    else:
+        return 0
