32da545b2fd098a8d4d8cb7b19f71c60fe5b0f63
Shobayo Samuel <shobayosamuel62@gmail.com>
2023-05-10 05:48:33 -0700

    fix bug

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
 # puppet script to authomate apache error
 
-exec = {'fix-line-error':
+exec {'fix-line-error':
   command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
   path    => ['/bin','/usr/bin']
 }
