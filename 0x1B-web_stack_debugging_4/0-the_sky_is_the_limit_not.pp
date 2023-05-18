812ca3acda6eb3ed1b9cbdcd9c3d03517e670943
Shobayo Samuel <shobayosamuel62@gmail.com>
2023-05-18 03:58:15 -0700

    fix: Increase the limit of requests

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,13 @@
+# puppet script to increase the amount of request nginx can handle
+
+# Increase the ULIMIT of nginx
+exec { 'Increase the limit':
+  command => 'sed -i "s/15/4096" /ets/default/nginx',
+  path    => '/usr/local/bin/:/bin/'
+}
+
+# restart my nginx
+-> exec { 'restart nginx':
+  command => 'nginx restart',
+  path    => '/etc/init.d/'
+}
